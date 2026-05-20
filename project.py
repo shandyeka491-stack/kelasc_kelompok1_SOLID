856d0d339f3188bb6e15de194db6735854a45211
Thiraza Naufal <zhafranwindra@gmail.com>
2026-05-20 11:53:53 +0700

    tugas analisis

--- project.py
+++ project.py
@@ -1,5 +1,31 @@
-print("Hello World")
-print("Hello World")
-print("hi")
+class Hewan:
+    def __init__(self, nama, jenis):
+        self.nama = nama
+        self.jenis = jenis
 
-print("hi")
+    def makan(self):
+        print(f"{self.nama} sedang makan.")
+
+    def terbang(self):
+        print(f"{self.nama} sedang terbang.")
+
+
+class Kandang:
+    def __init__(self):
+        self.hewan_list = []
+
+    def tambah_hewan(self, hewan):
+        self.hewan_list.append(hewan)
+
+    def bersihkan_kandang(self):
+        print("Kandang dibersihkan.")
+
+
+class KebunBinatang:
+    def __init__(self):
+        self.kandang = Kandang()
+
+    def rawat_semua_hewan(self):
+        for hewan in self.kandang.hewan_list:
+            hewan.makan()
+            hewan.terbang()
